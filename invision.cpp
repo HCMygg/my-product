160dfd15b4803ba7f8f7c190538d9c07217000c2
Wang <wangmingze100@outlook.com>
2026-08-20 13:59:52 +0800

    写了个反转字符串

--- invision.cpp
+++ invision.cpp
@@ -1,12 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std; 
+void invision(char *b,int c)
+{
+    for(int i=0;i<=c/2;i++)
+    {
+        swap(*(b+i),*(b+(c-i-1)));
+    }
+}
 int main()
 {
     string a;
-    cin>>a;
+    getline(cin,a);
     char *b;
     b=&a[0];
     int c;
     c=strlen(b);
-    cout<<c;
+    invision(b,c);
+    cout<<b;
 }
